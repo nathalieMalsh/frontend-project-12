5e5abaf90c74aadfff69682f0754d1e807010505
nmalysheva99@mail.ru <nathalie_malsh@LAPTOP-HHAP2AP9.localdomain>
2025-08-08 14:39:32 +0300

    step 3, task 3 - done

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,7 +1,23 @@
-const MainPage = () => (
-  <div>
-    <h1>Главная страница</h1>
-  </div>
-)
+import React from 'react';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const MainPage = () => {
+  const navigate = useNavigate()
+
+  const token = window.localStorage.getItem('userId')
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+  }, [])
+
+  return (
+    <div>
+      <h1>Главная страница</h1>
+    </div>
+  )
+}
 
 export default MainPage;
